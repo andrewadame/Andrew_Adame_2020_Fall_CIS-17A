e495774da9d2dee1acc1513b6b5de6c7375fe355
andrewadame <67297166+andrewadame@users.noreply.github.com>
2020-08-25 15:43:55 -0700

    Update 3
    Another One

---
+++ Lab/IntroLab/Assignment#1_HelloWorld!/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * File:   main.cpp
+ * Author: Andrew Adame
+ * Title: HelloWorld!
+ * Program displays message, "Hello World!" when ran
+ *
+ * Created on August 25, 2020, 3:38 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    cout << "Hello World! :D" << endl;
+    cout << "from Andrew Adame" << endl;
+
+    return 0;
+}
+
---
+++ Lab/IntroLab/Assignment#1_HelloWorld!/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=assignment_1_helloworld_
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/assignment_1_helloworld_
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=assignment#1helloworld!.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/assignment#1helloworld!.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=assignment_1_helloworld_
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/assignment_1_helloworld_
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=assignment#1helloworld!.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/assignment#1helloworld!.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
