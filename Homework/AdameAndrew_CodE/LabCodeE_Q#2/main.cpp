f10a255e8e67365f22536ed46e5c735476797288
andrewadame <67297166+andrewadame@users.noreply.github.com>
2020-09-07 09:34:39 -0700

    Update
    Another one

---
+++ Homework/AdameAndrew_CodE/LabCodeE_Q#2/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * Author: 
+ * Date:
+ * Purpose:
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) 
+{
+    cout << endl;
+    cout << "   Hello World!  " << endl;
+    cout << endl;
+    return 0;
+}
